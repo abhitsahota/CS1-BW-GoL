c22917250aed962976da6b1fa252354fcc8ab254
abhitsahota <abhit.sahota@gmail.com>
2020-08-27 21:48:32 -0400

    Updates

---
+++ precache-manifest.1d6775f8fc4ecbaf475883229e270748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1f766b4adcfe8032333db7ff64df49",
+    "url": "/CS1-BW-GoL/index.html"
+  },
+  {
+    "revision": "d8ca4d6101c4899fd849",
+    "url": "/CS1-BW-GoL/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3c3b5e7818840716076",
+    "url": "/CS1-BW-GoL/static/js/2.5b12214d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CS1-BW-GoL/static/js/2.5b12214d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ca4d6101c4899fd849",
+    "url": "/CS1-BW-GoL/static/js/main.9672b388.chunk.js"
+  },
+  {
+    "revision": "1b4c3b0d91b274724d18",
+    "url": "/CS1-BW-GoL/static/js/runtime-main.f3bf8ee8.js"
+  }
+]);
